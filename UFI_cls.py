e51cfd78ab563f44be1b8316103983474043542c
Zhengze Zhou <33163690+ZhengzeZhou@users.noreply.github.com>
2019-04-10 15:16:04 -0400

    Add files via upload

--- UFI_cls.py
+++ UFI_cls.py
@@ -27,26 +27,33 @@ def UFI_cls(self, X, y):
 
         n_nodes = tree.tree_.node_count
 
-        tree_X_inbag = X.repeat((self.inbag_times_[:, index]).astype("int"), axis = 0)
-        tree_y_inbag = y.repeat((self.inbag_times_[:, index]).astype("int"), axis = 0)
-        decision_path_inbag = tree.decision_path(tree_X_inbag).todense()
+        tree_X_inb = X.repeat((self.inbag_times_[:, index]).astype("int"), axis = 0)
+        tree_y_inb = y.repeat((self.inbag_times_[:, index]).astype("int"), axis = 0)
+        decision_path_inb = tree.decision_path(tree_X_inb).todense()
 
-        outofbag_index = (self.inbag_times_[:, index] == 0)
-        tree_X = X[outofbag_index]
-        tree_y = y[outofbag_index]
-        decision_path = tree.decision_path(tree_X).todense()
+        oob_index = (self.inbag_times_[:, index] == 0)
+        tree_X_oob = X[oob_index]
+        tree_y_oob = y[oob_index]
+        decision_path_oob = tree.decision_path(tree_X_oob).todense()
 
         impurity = [0] * n_nodes
 
-        weighted_n_node_samples = np.array(np.sum(decision_path, axis = 0))[0]
+        flag = [True] * n_nodes
+
+        weighted_n_node_samples = np.array(np.sum(decision_path_inb, axis = 0))[0]
 
         for i in range(n_nodes):
 
-            arr1 = tree_y[np.array(decision_path[:, i]).ravel().nonzero()[0].tolist()]
-            arr2 = tree_y_inbag[np.array(decision_path_inbag[:, i]).ravel().nonzero()[0].tolist()]
+            arr1 = tree_y_oob[np.array(decision_path_oob[:, i]).ravel().nonzero()[0].tolist()]
+            arr2 = tree_y_inb[np.array(decision_path_inb[:, i]).ravel().nonzero()[0].tolist()]
 
-            if len(arr1) == 0 or len(arr2) == 0:
-                impurity[i] = 0
+            if len(arr1) == 0:
+                if sum(tree.tree_.children_left == i) > 0:
+                    parent_node = np.arange(n_nodes)[tree.tree_.children_left == i][0]
+                    flag[parent_node] = False
+                else:
+                    parent_node = np.arange(n_nodes)[tree.tree_.children_right == i][0]
+                    flag[parent_node] = False
             else:
                 p1 = float(sum(arr1)) / len(arr1)
                 pp1 = float(sum(arr2)) / len(arr2)
@@ -65,13 +72,15 @@ def UFI_cls(self, X, y):
             node_left = tree.tree_.children_left[node]
             node_right = tree.tree_.children_right[node]
 
+            if flag[node] == True:
+                
             incre = (weighted_n_node_samples[node] * impurity[node] -
                                 weighted_n_node_samples[node_left] * impurity[node_left] -
                                 weighted_n_node_samples[node_right] * impurity[node_right])
 
             temp[v] += incre
 
-        VI += temp / len(tree_y)
+        VI += temp
 
     return VI / n_estimators
     
